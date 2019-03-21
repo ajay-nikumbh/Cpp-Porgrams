e83c76fe1310bebdda28e3ce253311ddd24c12dc
Ajay Nikumbh <37560890+ajay-nikumbh@users.noreply.github.com>
2019-03-21 10:47:29 +0530

    Create addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout << "Enter the 2 nos";
+    cin >>a>>b;
+    c=a+b;
+    cout <<"Sum is:"<<c<<endl;
+
+
+    return 0;
+}

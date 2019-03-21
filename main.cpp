12eba6924b9c8c0933da4fbd7063b79e6510ec49
ajay-nikumbh <ajaynikumbh.1@gmail.com>
2019-03-21 10:45:08 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Size of the character and min and max values
+    char c;
+    cout <<sizeof(c)<<endl;
+    cout<<CHAR_MIN<<"  "<<CHAR_MAX;
+
+    //Size of the integer anmd min and max values
+    int i;
+    cout<<sizeof(i)<<endl;
+    cout<<INT_MIN<<"  "<<INT_MAX;
+
+
+    return 0;
+}
